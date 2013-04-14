a9d842a33bed1a526b8fb77863a20881b0ca07a8
sauerbraten <willing.alexander@googlemail.com>
2013-04-14 15:40:53 +0200

    simplified the API type; made handler functions' definitions package-global; made storage variable package-global, too

--- api.go
+++ api.go
@@ -15,40 +15,23 @@ import (
 )
 
 type apiResponse struct {
-	Error    string      `json:"error,omitempty"`
-	Id       string      `json:"id,omitempty"`
-	Resource interface{} `json:"resource,omitempty"`
+	Error  string      `json:"error,omitempty"`
+	Id     string      `json:"id,omitempty"`
+	Result interface{} `json:"result,omitempty"`
 }
+
+var s Storage
 
 // API exposes the CRUD handlers.
 type API struct {
-	// the API's storage
-	s Storage
-
-	// Create is meant to handle POST requests. It returns '400 Bad Request', '404 Not Found' or '201 Created'.
-	create func(resp http.ResponseWriter, req *http.Request)
-
-	// Get is meant to retrieve resources (HTTP GET). It returns '404 Not Found' or '200 OK'.
-	get func(resp http.ResponseWriter, req *http.Request)
-
-	// GetAll returns all resources of a kind.
-	getAll func(resp http.ResponseWriter, req *http.Request)
-
-	// Update is meant to handle PUTs. It returns '400 Bad Request', '404 Not Found' or '200 OK'.
-	update func(resp http.ResponseWriter, req *http.Request)
-
-	// Delete is for handling DELETE requests on single resources. Possible HTTP status codes are '404 Not Found' and '200 OK'.
-	delete func(resp http.ResponseWriter, req *http.Request)
-
-	// DeleteAll is for handling DELETE on whole kinds. Possible HTTP status codes are '404 Not Found' and '200 OK'.
-	deleteAll func(resp http.ResponseWriter, req *http.Request)
-
 	// The generated CRUD routes. Pass this to http.ListenAndServe().
 	Router *mux.Router
 }
 
 // Returns an API relying on the given Storage. The path prefix must start with '/' and be 2 or more characters long. If those criteria are not met, "/api" is used as a fallback instead. Trailing slashes are stripped, like so: "/api///" → "/api".
-func NewAPI(pathPrefix string, s Storage) *API {
+func NewAPI(pathPrefix string, sto Storage) *API {
+	s = sto
+
 	// validate path prefix
 	if len(pathPrefix) < 2 || pathPrefix[0] != '/' {
 		pathPrefix = "/api"
@@ -59,212 +42,209 @@ func NewAPI(pathPrefix string, s Storage) *API {
 		pathPrefix = pathPrefix[:len(pathPrefix)-1]
 	}
 
-	api := &API{}
-
-	api.s = s
-
-	api.create = func(resp http.ResponseWriter, req *http.Request) {
-		vars := mux.Vars(req)
-		kind := vars["kind"]
-		enc := json.NewEncoder(resp)
-		dec := json.NewDecoder(req.Body)
-
-		// read body and parse into interface{}
-		var resource map[string]interface{}
-		err := dec.Decode(&resource)
-
-		if err != nil {
-			log.Println(err)
-			resp.WriteHeader(400) // Bad Request
-			enc.Encode(apiResponse{"malformed json", "", nil})
-			return
-		}
-
-		// set in storage
-		id, stoErr := s.Create(kind, resource)
-
-		// handle error
-		switch stoErr {
-		case KindNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"kind not found", "", nil})
-			return
-		case InternalError:
-			resp.WriteHeader(500) // Internal Server Error
-			enc.Encode(apiResponse{"storage failure", "", nil})
-			return
-		}
-
-		// report success
-		resp.WriteHeader(201) // Created
-		enc.Encode(apiResponse{"", id, nil})
-	}
-
-	api.get = func(resp http.ResponseWriter, req *http.Request) {
-		vars := mux.Vars(req)
-		kind := vars["kind"]
-		id := vars["id"]
-		enc := json.NewEncoder(resp)
-
-		// look for resource
-		resource, stoErr := s.Get(kind, id)
-
-		// handle error
-		switch stoErr {
-		case KindNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"kind not found", "", nil})
-			return
-		case ResourceNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"resource not found", "", nil})
-			return
-		case InternalError:
-			resp.WriteHeader(500) // Internal Server Error
-			enc.Encode(apiResponse{"storage failure", "", nil})
-			return
-		}
-
-		// return resource
-		err := enc.Encode(apiResponse{"", "", resource})
-		if err != nil {
-			log.Println(err)
-		}
-	}
-
-	api.getAll = func(resp http.ResponseWriter, req *http.Request) {
-		vars := mux.Vars(req)
-		kind := vars["kind"]
-		enc := json.NewEncoder(resp)
-
-		// look for resources
-		resources, stoErr := s.GetAll(kind)
-
-		// handle error
-		switch stoErr {
-		case KindNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"kind not found", "", nil})
-			return
-		case InternalError:
-			resp.WriteHeader(500) // Internal Server Error
-			enc.Encode(apiResponse{"storage failure", "", nil})
-			return
-		}
-
-		// return resources
-		err := enc.Encode(apiResponse{"", "", resources})
-		if err != nil {
-			log.Println(err)
-		}
-	}
-
-	api.update = func(resp http.ResponseWriter, req *http.Request) {
-		vars := mux.Vars(req)
-		kind := vars["kind"]
-		id := vars["id"]
-		enc := json.NewEncoder(resp)
-		dec := json.NewDecoder(req.Body)
-
-		// read body and parse into interface{}
-		var resource map[string]interface{}
-		err := dec.Decode(&resource)
-
-		if err != nil {
-			log.Println(err)
-			resp.WriteHeader(400) // Bad Request
-			enc.Encode(apiResponse{"malformed json", "", nil})
-			return
-		}
-
-		// update resource
-		stoErr := s.Update(kind, id, resource)
-
-		// handle error
-		switch stoErr {
-		case KindNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"kind not found", "", nil})
-			return
-		case ResourceNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"resource not found", "", nil})
-			return
-		case InternalError:
-			resp.WriteHeader(500) // Internal Server Error
-			enc.Encode(apiResponse{"storage failure", "", nil})
-			return
-		}
-
-		// 200 OK is implied
-		enc.Encode(apiResponse{"", "", nil})
-	}
-
-	api.delete = func(resp http.ResponseWriter, req *http.Request) {
-		vars := mux.Vars(req)
-		kind := vars["kind"]
-		id := vars["id"]
-		enc := json.NewEncoder(resp)
-
-		// delete resource
-		stoErr := s.Delete(kind, id)
-
-		// handle error
-		switch stoErr {
-		case KindNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"kind not found", "", nil})
-			return
-		case ResourceNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"resource not found", "", nil})
-			return
-		case InternalError:
-			resp.WriteHeader(500) // Internal Server Error
-			enc.Encode(apiResponse{"storage failure", "", nil})
-			return
-		}
-
-		// 200 OK is implied
-		enc.Encode(apiResponse{"", "", nil})
-	}
-
-	api.deleteAll = func(resp http.ResponseWriter, req *http.Request) {
-		vars := mux.Vars(req)
-		kind := vars["kind"]
-		enc := json.NewEncoder(resp)
-
-		// look for resources
-		stoErr := s.DeleteAll(kind)
-
-		// handle error
-		switch stoErr {
-		case KindNotFound:
-			resp.WriteHeader(404) // Not Found
-			enc.Encode(apiResponse{"kind not found", "", nil})
-			return
-		case InternalError:
-			resp.WriteHeader(500) // Internal Server Error
-			enc.Encode(apiResponse{"storage failure", "", nil})
-			return
-		}
-
-		// 200 OK is implied
-		enc.Encode(apiResponse{"", "", nil})
-	}
-
-	api.Router = mux.NewRouter()
+	api := &API{mux.NewRouter()}
 	api.Router.StrictSlash(true)
 
 	r := api.Router.PathPrefix(pathPrefix).Subrouter()
 
 	// set up CRUD routes
-	r.HandleFunc("/{kind}", api.create).Methods("POST")
-	r.HandleFunc("/{kind}", api.getAll).Methods("GET")
-	r.HandleFunc("/{kind}/{id}", api.get).Methods("GET")
-	r.HandleFunc("/{kind}/{id}", api.update).Methods("PUT")
-	r.HandleFunc("/{kind}", api.deleteAll).Methods("DELETE")
-	r.HandleFunc("/{kind}/{id}", api.delete).Methods("DELETE")
+	r.HandleFunc("/{kind}", create).Methods("POST")
+	r.HandleFunc("/{kind}", getAll).Methods("GET")
+	r.HandleFunc("/{kind}/{id}", get).Methods("GET")
+	r.HandleFunc("/{kind}/{id}", update).Methods("PUT")
+	r.HandleFunc("/{kind}", deleteAll).Methods("DELETE")
+	r.HandleFunc("/{kind}/{id}", del).Methods("DELETE")
 
 	return api
+}
+
+func create(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	kind := vars["kind"]
+	enc := json.NewEncoder(resp)
+	dec := json.NewDecoder(req.Body)
+
+	// read body and parse into interface{}
+	var resource map[string]interface{}
+	err := dec.Decode(&resource)
+
+	if err != nil {
+		log.Println(err)
+		resp.WriteHeader(400) // Bad Request
+		enc.Encode(apiResponse{"malformed json", "", nil})
+		return
+	}
+
+	// set in storage
+	id, stoErr := s.Create(kind, resource)
+
+	// handle error
+	switch stoErr {
+	case KindNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"kind not found", "", nil})
+		return
+	case InternalError:
+		resp.WriteHeader(500) // Internal Server Error
+		enc.Encode(apiResponse{"storage failure", "", nil})
+		return
+	}
+
+	// report success
+	resp.WriteHeader(201) // Created
+	enc.Encode(apiResponse{"", id, nil})
+}
+
+func get(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	kind := vars["kind"]
+	id := vars["id"]
+	enc := json.NewEncoder(resp)
+
+	// look for resource
+	resource, stoErr := s.Get(kind, id)
+
+	// handle error
+	switch stoErr {
+	case KindNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"kind not found", "", nil})
+		return
+	case ResourceNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"resource not found", "", nil})
+		return
+	case InternalError:
+		resp.WriteHeader(500) // Internal Server Error
+		enc.Encode(apiResponse{"storage failure", "", nil})
+		return
+	}
+
+	// return resource
+	err := enc.Encode(apiResponse{"", "", resource})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func getAll(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	kind := vars["kind"]
+	enc := json.NewEncoder(resp)
+
+	// look for resources
+	resources, stoErr := s.GetAll(kind)
+
+	// handle error
+	switch stoErr {
+	case KindNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"kind not found", "", nil})
+		return
+	case InternalError:
+		resp.WriteHeader(500) // Internal Server Error
+		enc.Encode(apiResponse{"storage failure", "", nil})
+		return
+	}
+
+	// return resources
+	err := enc.Encode(apiResponse{"", "", resources})
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func update(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	kind := vars["kind"]
+	id := vars["id"]
+	enc := json.NewEncoder(resp)
+	dec := json.NewDecoder(req.Body)
+
+	// read body and parse into interface{}
+	var resource map[string]interface{}
+	err := dec.Decode(&resource)
+
+	if err != nil {
+		log.Println(err)
+		resp.WriteHeader(400) // Bad Request
+		enc.Encode(apiResponse{"malformed json", "", nil})
+		return
+	}
+
+	// update resource
+	stoErr := s.Update(kind, id, resource)
+
+	// handle error
+	switch stoErr {
+	case KindNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"kind not found", "", nil})
+		return
+	case ResourceNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"resource not found", "", nil})
+		return
+	case InternalError:
+		resp.WriteHeader(500) // Internal Server Error
+		enc.Encode(apiResponse{"storage failure", "", nil})
+		return
+	}
+
+	// 200 OK is implied
+	enc.Encode(apiResponse{"", "", nil})
+}
+
+// delete() is a built-in function for maps, thus the shorthand name 'del' for this handler function
+func del(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	kind := vars["kind"]
+	id := vars["id"]
+	enc := json.NewEncoder(resp)
+
+	// delete resource
+	stoErr := s.Delete(kind, id)
+
+	// handle error
+	switch stoErr {
+	case KindNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"kind not found", "", nil})
+		return
+	case ResourceNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"resource not found", "", nil})
+		return
+	case InternalError:
+		resp.WriteHeader(500) // Internal Server Error
+		enc.Encode(apiResponse{"storage failure", "", nil})
+		return
+	}
+
+	// 200 OK is implied
+	enc.Encode(apiResponse{"", "", nil})
+}
+
+func deleteAll(resp http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	kind := vars["kind"]
+	enc := json.NewEncoder(resp)
+
+	// look for resources
+	stoErr := s.DeleteAll(kind)
+
+	// handle error
+	switch stoErr {
+	case KindNotFound:
+		resp.WriteHeader(404) // Not Found
+		enc.Encode(apiResponse{"kind not found", "", nil})
+		return
+	case InternalError:
+		resp.WriteHeader(500) // Internal Server Error
+		enc.Encode(apiResponse{"storage failure", "", nil})
+		return
+	}
+
+	// 200 OK is implied
+	enc.Encode(apiResponse{"", "", nil})
 }
--- storage.go
+++ storage.go
@@ -1,11 +1,5 @@
 package crudapi
 
-import (
-	"strconv"
-	"sync"
-	"time"
-)
-
 // A StorageError is returned by Storage's methods and describes what kind of error occured.
 type StorageError int
 
@@ -23,200 +17,23 @@ const (
 type Storage interface {
 	// first argument is always the kind of resource (for example the database table to use)
 	// a second string argument is the resource ID
-	// a map[string]interface{} is a resource (for example a JSON object or a database row with map indexes ~ column names)
+	// a interface{} is a resource (for example a JSON object or a database row with map indexes ~ column names)
 	//
 	// creates a resource and stores the data in it, then returns the ID
-	Create(string, map[string]interface{}) (string, StorageError)
+	Create(string, interface{}) (string, StorageError)
 
 	// retrieves a resource
-	Get(string, string) (map[string]interface{}, StorageError)
+	Get(string, string) (interface{}, StorageError)
 
 	// retrieves all resources of the specified kind
-	GetAll(string) ([]map[string]interface{}, StorageError)
+	GetAll(string) ([]interface{}, StorageError)
 
 	// updates a resource
-	Update(string, string, map[string]interface{}) StorageError
+	Update(string, string, interface{}) StorageError
 
 	// deletes a resource
 	Delete(string, string) StorageError
 
 	// delete all resources of a kind
 	DeleteAll(string) StorageError
-}
-
-// MapStorage is a basic storage using maps. Thus, it is not persistent! It is meant as an example and for testing purposes.
-// MapStorage is thread-safe, as any Storage implementation should be, since CRUD handlers run in parrallel as well.
-type MapStorage struct {
-	sync.RWMutex
-	data map[string]map[string]map[string]interface{}
-}
-
-// Returns an initialized MapStorage
-func NewMapStorage() MapStorage {
-	return MapStorage{sync.RWMutex{}, make(map[string]map[string]map[string]interface{})}
-}
-
-// Adds a map[string]interface{} to the root level map. Equivalent to a database table.
-func (ms MapStorage) AddMap(kind string) StorageError {
-	// check if kind already exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if ok {
-		return KindExists
-	}
-
-	ms.Lock()
-	ms.data[kind] = make(map[string]map[string]interface{})
-	ms.Unlock()
-
-	return None
-}
-
-// Reverts AddMap().
-func (ms MapStorage) DeleteMap(kind string) StorageError {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		return KindNotFound
-	}
-
-	ms.Lock()
-	delete(ms.data, kind)
-	ms.Unlock()
-
-	return None
-}
-
-func (ms MapStorage) Create(kind string, resource map[string]interface{}) (id string, err StorageError) {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		return
-	}
-
-	// make (pesudo-random) ID
-	id = strconv.FormatInt(time.Now().Unix(), 10)
-
-	// create nil entry for the new ID
-	ms.Lock()
-	ms.data[kind][id] = resource
-	ms.Unlock()
-
-	return
-}
-
-func (ms MapStorage) Get(kind, id string) (resource map[string]interface{}, err StorageError) {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		err = KindNotFound
-		return
-	}
-
-	// make sure a resource with this ID exists
-	ms.RLock()
-	resource, ok = ms.data[kind][id]
-	ms.RUnlock()
-	if !ok {
-		err = ResourceNotFound
-		return
-	}
-
-	return
-}
-
-func (ms MapStorage) GetAll(kind string) (resources []map[string]interface{}, err StorageError) {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		err = KindNotFound
-		return
-	}
-
-	// collect all values in the kind's map in a slice
-	ms.RLock()
-	for _, resource := range ms.data[kind] {
-		resources = append(resources, resource)
-	}
-	ms.RUnlock()
-
-	return
-}
-
-func (ms MapStorage) Update(kind, id string, resource map[string]interface{}) StorageError {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		return KindNotFound
-	}
-
-	// make sure the resource exists
-	ms.RLock()
-	_, ok = ms.data[kind][id]
-	ms.RUnlock()
-	if !ok {
-		return ResourceNotFound
-	}
-
-	// update resource
-	ms.Lock()
-	ms.data[kind][id] = resource
-	ms.Unlock()
-
-	return None
-}
-
-func (ms MapStorage) Delete(kind, id string) StorageError {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		return KindNotFound
-	}
-
-	// make sure the resource exists
-	ms.RLock()
-	_, ok = ms.data[kind][id]
-	ms.RUnlock()
-	if !ok {
-		return ResourceNotFound
-	}
-
-	// delete resource
-	ms.Lock()
-	delete(ms.data[kind], id)
-	ms.Unlock()
-
-	return None
-}
-
-func (ms MapStorage) DeleteAll(kind string) StorageError {
-	// make sure kind exists
-	ms.RLock()
-	_, ok := ms.data[kind]
-	ms.RUnlock()
-	if !ok {
-		return KindNotFound
-	}
-
-	// delete resources
-	ms.Lock()
-	for id := range ms.data[kind] {
-		delete(ms.data[kind], id)
-	}
-	ms.Unlock()
-
-	return None
 }
